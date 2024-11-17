14756d135ba72a956133450c896a5f5353474404
juanvel4000 <juanvel400@proton.me>
2024-11-16 19:17:23 -0500

    Added more echo-ing to install.sh

--- install.sh
+++ install.sh
@@ -7,12 +7,16 @@ else
 	exit 1
 fi
 if [ "$1" = "uninstall" ]; then
+	echo "Removing Arcabox Directories"
 	rm -rf /usr/share/arcabox
+	echo "Removing the Arcabox script"
 	rm -rf /usr/local/bin/arcabox
+	echo "Done."
 	exit 1
 elif [ "$1" = "install" ]; then
-	
+	echo "Installing Arcabox to /usr/local/bin"
 	cp arcabox /usr/local/bin
+	echo "Creating a Arcabox directory"
 	mkdir -p /usr/share/arcabox
 	exit 1
 else
