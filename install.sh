9d25c9b33bfc677d000e16666005c7461a196c31
juanvel4000 <juanvel400@proton.me>
2024-11-24 11:26:40 -0500

    Improved install.sh

--- install.sh
+++ install.sh
@@ -1,44 +1,49 @@
 #!/bin/bash
-# Simple Install Script for Arcabox
-
 set -e
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 RESET='\033[0m'  
 BOLD='\033[1m'   
 
-
-if [ "$UID" -ne 0 ]; then
+if [ "$UID" -ne 0 ] && [ -z "$DESTDIR" ]; then
     echo -e "${BOLD}${RED}=> ${RESET}${BOLD}Please run as root${RESET}"
     exit 1
 fi
 
-
 if [ "$1" = "uninstall" ]; then
-	echo -e "${BOLD}${GREEN}=> ${RESET}${BOLD}Removing Arcabox...${RESET}"
-	rm -rf /usr/share/arcabox
-	rm -rf /usr/local/bin/arcabox
-	echo -e "${BOLD}${GREEN}=> ${RESET}${BOLD} Done.${RESET}"
-	exit 1
-elif [ "$1" = "install" ]; then
-if command -v curl >/dev/null 2>&1; then
-    echo -e "${BOLD}${GREEN}=>${RESET}${BOLD} Found cURL${RESET}"
-else
-    echo -e "${BOLD}${RED}=>${RESET}${BOLD} Please install cURL to continue ${RESET}"
-	exit
+    echo -e "${BOLD}${GREEN}=> ${RESET}${BOLD}Removing Arcabox...${RESET}"
+    rm -rf /usr/share/arcabox
+    rm -rf /usr/local/bin/arcabox
+    echo -e "${BOLD}${GREEN}=> ${RESET}${BOLD}Done.${RESET}"
+    exit 0
 fi
-if command -v tar >/dev/null 2>&1; then
-    echo -e "${BOLD}${GREEN}=>${RESET}${BOLD} Found tar${RESET}"
-else
-    echo -e "${BOLD}${RED}=>${RESET}${BOLD} Please install tar to continue ${RESET}"
-	exit
-fi
-	echo -e "${BOLD}${GREEN}=> ${RESET}${BOLD}Installing Arcabox to /usr/local/bin${RESET}"
-	cp arcabox /usr/local/bin
-	mkdir -p /usr/share/arcabox
-	echo -e "${BOLD}${GREEN}=> ${RESET}${BOLD}Done!${RESET}"
 
-else
-	echo -e "${BOLD}${RED}=> ${RESET}${BOLD} Usage: install.sh <install|uninstall>${RESET}"
+if [ "$1" = "install" ]; then
+    if command -v curl >/dev/null 2>&1; then
+        echo -e "${BOLD}${GREEN}=>${RESET}${BOLD} Found cURL${RESET}"
+    else
+        echo -e "${BOLD}${RED}=>${RESET}${BOLD} Please install cURL to continue ${RESET}"
+        exit 1
+    fi
+
+    if command -v tar >/dev/null 2>&1; then
+        echo -e "${BOLD}${GREEN}=>${RESET}${BOLD} Found tar${RESET}"
+    else
+        echo -e "${BOLD}${RED}=>${RESET}${BOLD} Please install tar to continue ${RESET}"
+        exit 1
+    fi
+
+    INSTALL_DIR="${DESTDIR:-/usr/local/bin}"
+    echo -e "${BOLD}${GREEN}=> ${RESET}${BOLD}Installing Arcabox to $INSTALL_DIR${RESET}"
+    cp arcabox "$INSTALL_DIR"
+
+    INSTALL_SHARE_DIR="${DESTDIR:-/usr/share}/arcabox"
+    mkdir -p "$INSTALL_SHARE_DIR"
+
+    echo -e "${BOLD}${GREEN}=> ${RESET}${BOLD}Done!${RESET}"
+    exit 0
 fi
+
+echo -e "${BOLD}${RED}=> ${RESET}${BOLD} Usage: install.sh <install|uninstall>${RESET}"
+exit 1
 
